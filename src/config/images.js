fc6707c8d78601327187ff2c8c07aee87b71c0f0
cloud0811 <aaa0926289869@gmail.com>
2022-06-28 20:08:54 +0800

    feat: clean-react-redux

---
+++ src/config/images.js
@@ -0,0 +1,11 @@
+/* App config for images
+ */
+// const IMAGE_FOLDER = "../assets/images/";
+
+const images = {
+  // NotFound
+  NotFound: require(`../assets/images/NotFound2.png`),
+  NotFoundBG: require(`../assets/images/NotFoundBG.png`),
+};
+
+export default images;
